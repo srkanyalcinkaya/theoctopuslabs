b55a3c29a03b113b0e2d6f9c98c45b55823ba2e5
SerkanYalçınkaya <syalcinkaya895@gmail.com>
2024-05-18 21:04:54 +0300

    update revalidate cache

--- app/lib/datocms.js
+++ app/lib/datocms.js
@@ -1,69 +1,3 @@
-// import { cache } from 'react';
-
-// const dedupedFetch = cache(
-//     async (
-//         body,
-//         includeDrafts = false,
-//         excludeInvalid = false,
-//         visualEditingBaseUrl = null,
-//         revalidate = null,
-//     ) => {
-//         const headers = {
-//             Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
-//             ...(includeDrafts ? { 'X-Include-Drafts': 'true' } : {}),
-//             ...(excludeInvalid ? { 'X-Exclude-Invalid': 'true' } : {}),
-//             ...(visualEditingBaseUrl
-//                 ? {
-//                     'X-Visual-Editing': 'vercel-v1',
-//                     'X-Base-Editing-Url': visualEditingBaseUrl,
-//                 }
-//                 : {}),
-//             ...(process.env.NEXT_DATOCMS_ENVIRONMENT
-//                 ? { 'X-Environment': process.env.NEXT_DATOCMS_ENVIRONMENT }
-//                 : {}),
-//         };
-
-//         const response =  await fetch('https://graphql.datocms.com/', {
-//             method: 'POST',
-//             headers,
-//             body,
-//             next: { revalidate },
-//         })
-//         console.log(response)
-//         const responseBody = await response.json();
-
-//         if (!response.ok) {
-//             throw new Error(
-//                 `${response.status} ${response.statusText}: ${JSON.stringify(
-//                     responseBody,
-//                 )}`,
-//             );
-//         }
-
-//         return responseBody;
-//     },
-// );
-
-// export async function performRequest({
-//     query,
-//     variables = {},
-//     includeDrafts = false,
-//     excludeInvalid = false,
-//     visualEditingBaseUrl,
-//     revalidate,
-// }) {
-//     const { data } = await dedupedFetch(
-//         JSON.stringify({ query, variables, revalidate }),
-//         includeDrafts,
-//         excludeInvalid,
-//         visualEditingBaseUrl,
-//         revalidate,
-//     );
-
-//     return data;
-// }
-
-
 import { cache } from 'react';
 
 const dedupedFetch = cache(
@@ -93,7 +27,7 @@ const dedupedFetch = cache(
             method: 'POST',
             headers,
             body,
-            next: { revalidate },
+            next: { revalidate: revalidate ? revalidate : 60 },
         })
         // console.log(response)
         const responseBody = await response.json();
@@ -116,7 +50,7 @@ export async function performRequest({
     includeDrafts = false,
     excludeInvalid = false,
     visualEditingBaseUrl,
-    revalidate = false, // 0 veya false olarak ayarlanıyor
+    revalidate = 60,
 }) {
     const { data } = await dedupedFetch(
         JSON.stringify({ query, variables, revalidate }),
