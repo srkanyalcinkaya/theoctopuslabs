d409eac265c9462dabbc429e5bd0435032dfeced
SerkanYalçınkaya <syalcinkaya895@gmail.com>
2024-05-18 03:44:13 +0300

    Added clients

--- app/components/home/clients/index.js
+++ app/components/home/clients/index.js
@@ -3,9 +3,29 @@ import Cards from "@/app/components/cards"
 const cardsData = [
     {
         title:"Chavinci Network",
-        description:"Payment platform that enables seamless transactions between blockchain and the banking system.",
+        description:"Chavinci is a Layer 1 blockchain network utilizing the Proof of Stake v3.0 consensus algorithm and Modular Ledger Technology, designed to cater to both end users and businesses.",
         img:"/images/cha.svg"
-    }
+    },
+    {
+        title:"LawLinkGlobal",
+        description:"LawLinkGlobal is an innovative legal service platform operating on blockchain, connecting users with a global network of attorneys and facilitating professional interactions among lawyers.",
+        img:"/images/cha.svg"
+    },
+    {
+        title:"Tabirle",
+        description:"Describe your dream and let Tabirle’s AI interpret its meaning. With AI, we provide comprehensive dream analysis and visualization, helping you remember and share your dreams.",
+        img:"/images/cha.svg"
+    },
+    {
+        title:"Salt and Partners",
+        description:"Salt & Partners pioneers a unique partnership model, uniting consultants and lawyers from around the world to deliver seamless cross-border legal and consultancy services, creating an international network.",
+        img:"/images/cha.svg"
+    },
+    {
+        title:"Genesis Law",
+        description:"Genesis Law is a leading firm offering legal advice and support in blockchain technology. We combine expertise in law, technology, and project management to help clients navigate blockchain interactions securely and efficiently.",
+        img:"/images/cha.svg"
+    },
 ]
 
 
@@ -14,7 +34,7 @@ export default function Clients() {
     return (
         <section id="clients" className='relative max-w-6xl mx-auto px-2 md:px-4 '>
 
-            <div className='pt-22  w-full  md:pt-28  flex flex-col justify-between'>
+            <div className='pt-22  w-full  md:pt-28  flex flex-col justify-between gap-8'>
                 <div className='pt-32 text-start'>
                     <h2 className='font-bold text-3xl md:text-5xl text-black '>Our clients</h2>
                 </div>
--- app/components/home/hero/index.js
+++ app/components/home/hero/index.js
@@ -2,13 +2,13 @@ export default function Hero() {
     return (
         <section className='w-full   px-2 md:px-4'>
 
-            <div className='flex flex-col md:flex-row items-center justify-between'>
+            <div className='flex flex-col md:flex-row items-center justify-between gap-8'>
                 <div className='pt-32 flex flex-col gap-4 items-start justify-start  max-w-3xl w-full'>
-                    <h1 className='font-syne font-bold text-3xl md:text-6xl text-black '>We bring your ideas to life in a stunning way.</h1>
-                    <p className='text-secondary text-sm md:text-lg  font-normal pt-4 tracking-[0.99px] '>We are here to help you make your vision a reality.</p>
+                    <h1 className='font-syne font-bold text-3xl md:text-6xl text-black '>Everything is Within Our Reach</h1>
+                    <p className='text-secondary text-sm md:text-lg  font-normal pt-4 tracking-[0.99px] '>At The Octopus Labs, we combine the strength of expertises—Software Engineering, Blockchain Development, Smart Contract Integration, AI Solutions, Mobile App Creation, Legal Expertise, Project Management, and User Experience (UX) Design—with three hearts of passion, dedication, and vision.</p>
                 </div>
                 <div>
-                    <img src="/images/hand.png" alt="Robot" className="   "/>
+                    <img src="/images/hand.png" alt="Robot"/>
                 </div>
             </div>
         </section>
