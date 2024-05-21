e320e127a952cae1936edccd52b384ed46c433dc
SerkanYalçınkaya <syalcinkaya895@gmail.com>
2024-05-21 17:38:28 +0300

    update language-selector components

--- app/components/language-selector/index.js
+++ app/components/language-selector/index.js
@@ -27,16 +27,16 @@ const LanguageSelector = ({ lng }) => {
                         setIsOpen(false)
                     }, 100)
                 }
-                className="ml-4 inline-flex w-28 items-center overflow-hidden rounded-md bg-white transition duration-100 hover:bg-gray-200 active:scale-95 active:bg-gray-300"
+                className="ml-4 inline-flex justify-center  w-28 items-center overflow-hidden rounded-md bg-white transition duration-100 hover:bg-gray-200 active:scale-95 active:bg-gray-300 "
             >
-                <button className="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-gray-800">
+                <button className="inline-flex cursor-pointer items-center text-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-gray-800">
                     {getLangNameFromCode(currentLocale)?.name || currentLocale}
                 </button>
             </div>
 
             <div
                 className={
-                    "absolute end-0 z-10 ml-4 mt-1 w-28 rounded-md border border-gray-100 bg-white shadow-lg" +
+                    "absolute end-0 z-10 ml-4 mt-1 w-full rounded-md border border-gray-100 bg-white shadow-lg" +
                     (isOpen ? "" : " hidden")
                 }
                 role="menu"
@@ -45,11 +45,11 @@ const LanguageSelector = ({ lng }) => {
                     return (
                         <div
                             key={locale}
-                            className="inline-flex w-full cursor-pointer items-end justify-start rounded-lg text-sm font-medium text-gray-900 hover:bg-gray-100"
+                            className="inline-flex w-full cursor-pointer items-end justify-start rounded- text-sm font-medium text-gray-900 hover:bg-gray-100"
                         >
                             <a
                                 href={"/" + locale + "/" + pathString}
-                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                                className="block px-4 py-2 text-sm text-gray-700 w-full text-center rounded-lg  hover:bg-gray-600  hover:text-white"
                                 role="menuitem"
                             >
                                 <div className="inline-flex">
